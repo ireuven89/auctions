840b1e5e74d0dccffdaccb16dfb82e08eff509bf
ireuven89 <aitzik89@gmail.com>
2025-06-26 14:17:47 +0300

    added auction creation validation

--- auction-service/internal/service.go
+++ auction-service/internal/service.go
@@ -3,6 +3,7 @@ package internal
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
@@ -38,7 +39,7 @@ func (s *AuctionService) Fetch(ctx context.Context, id string) (*auction.Auction
 
 	if err != nil {
 		s.logger.Error("AuctionService failed to fetch auction", zap.Error(err))
-		if err == sql.ErrNoRows {
+		if errors.Is(err, sql.ErrNoRows) {
 			return nil, auction.ErrNotFound
 		}
 		return nil, fmt.Errorf("AuctionService.Fetch failed fetching bidder %w", err)
@@ -48,7 +49,6 @@ func (s *AuctionService) Fetch(ctx context.Context, id string) (*auction.Auction
 }
 
 func (s *AuctionService) Search(ctx context.Context, request auction.AuctionRequest) ([]auction.Auction, error) {
-
 	res, err := s.repo.FindAll(ctx, request)
 
 	if err != nil {
@@ -70,6 +70,10 @@ func (s *AuctionService) Update(ctx context.Context, auction auction.AuctionRequ
 }
 
 func (s *AuctionService) Create(ctx context.Context, auction auction.AuctionRequest) (string, error) {
+	if err := validateAuction(auction); err != nil {
+		return "", err
+	}
+
 	auction.ID = generateID()
 
 	if err := s.repo.Create(ctx, auction); err != nil {
@@ -80,13 +84,20 @@ func (s *AuctionService) Create(ctx context.Context, auction auction.AuctionRequ
 	return auction.ID, nil
 }
 
+func validateAuction(auction auction.AuctionRequest) error {
+	if auction.Item == "" {
+		return fmt.Errorf("AuctionService.ValidateAuction failed invalid auction item")
+	}
+
+	return nil
+}
+
 func generateID() string {
 
 	return uuid.New().String()
 }
 
 func (s *AuctionService) Delete(ctx context.Context, id string) error {
-
 	if err := s.repo.Delete(ctx, id); err != nil {
 		s.logger.Error("AuctionService.Delete failed deleting bidder", zap.Error(err))
 		return fmt.Errorf("AuctionService.Delete failed deleting %w", err)
